9f3180253edf1653ba95d666cd42c8914443dc8f
Ajay Gangwar <ajayapril1991@email.com>
2026-09-04 11:33:33 +0530

    Added Switch and Single-Pass Appraoch

--- LeetCode/LeetCode_P13_RomanToInt/Program.cs
+++ LeetCode/LeetCode_P13_RomanToInt/Program.cs
@@ -2,6 +2,17 @@
 
 class Program
 {
+    private static readonly Dictionary<char, int> RomanValues = new()
+    {
+        { 'I', 1 },
+        { 'V', 5 },
+        { 'X', 10 },
+        { 'L', 50 },
+        { 'C', 100 },
+        { 'D', 500 },
+        { 'M', 1000 }
+    };
+
     /// <summary>
     /// Leet Code 13 - Roman to Integer
     /// </summary>
@@ -9,29 +20,30 @@ class Program
     static void Main(string[] args)
     {
         string input1 = "III";
-        Console.WriteLine(RomanToInt(input1));
+        Console.WriteLine(RomanToInt1(input1));
+        Console.WriteLine(RomanToInt2(input1));
+        Console.WriteLine(RomanToInt3(input1));
 
         string input2 = "LVIII";
-        Console.WriteLine(RomanToInt(input2));
+        Console.WriteLine(RomanToInt1(input2));
+        Console.WriteLine(RomanToInt2(input2));
+        Console.WriteLine(RomanToInt3(input2));
 
         string input3 = "MCMXCIV";
-        Console.WriteLine(RomanToInt(input3));
+        Console.WriteLine(RomanToInt1(input3));
+        Console.WriteLine(RomanToInt2(input3));
+        Console.WriteLine(RomanToInt3(input3));
     }
 
-    public static int RomanToInt(string s)
+    /// <summary>
+    /// Dictionary approach
+    /// </summary>
+    /// <param name="s"></param>
+    /// <returns></returns>
+    public static int RomanToInt1(string s)
     {
-        Dictionary<Char, int> RomanValues = new()
-        {
-            { 'I', 1 },
-            { 'V', 5 },
-            { 'X', 10 },
-            { 'L', 50 },
-            { 'C', 100 },
-            { 'D', 500 },
-            { 'M', 1000 }
-        };
-
         int result = 0;
+
         for (int i = 0; i < s.Length - 1; i++)
         {
             if (RomanValues[s[i]] >= RomanValues[s[i + 1]])
@@ -43,7 +55,77 @@ class Program
                 result -= RomanValues[s[i]];
             }
         }
-        result += RomanValues[s[^1]];
+
+        result += RomanValues[s[^1]]; // s[s.Length - 1] can be simplified using the C# index-from-end operator: s[^1]
+
+        return result;
+    }
+
+    /// <summary>
+    /// Switch expression
+    /// </summary>
+    /// <param name="s"></param>
+    /// <returns></returns>
+    public static int RomanToInt2(string s)
+    {
+        int result = 0;
+
+        for (int i = 0; i < s.Length; i++)
+        {
+            int current = GetValue(s[i]);
+
+            if (i + 1 < s.Length && current < GetValue(s[i + 1]))
+            {
+                result -= current;
+            }
+            else
+            {
+                result += current;
+            }
+        }
+
+        return result;
+    }
+
+    private static int GetValue(char c)
+    {
+        return c switch
+        {
+            'I' => 1,
+            'V' => 5,
+            'X' => 10,
+            'L' => 50,
+            'C' => 100,
+            'D' => 500,
+            'M' => 1000,
+            _ => 0
+        };
+    }
+
+    /// <summary>
+    /// Single-pass with Dictionary
+    /// </summary>
+    /// <param name="s"></param>
+    /// <returns></returns>
+    public static int RomanToInt3(string s)
+    {
+        int result = 0;
+
+        for (int i = 0; i < s.Length; i++)
+        {
+            int current = RomanValues[s[i]];
+
+            if (i + 1 < s.Length &&
+                current < RomanValues[s[i + 1]])
+            {
+                result -= current;
+            }
+            else
+            {
+                result += current;
+            }
+        }
+
         return result;
     }
 }
